8856047b7859ee408c53407906c6e509c79dfbd1
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-29 20:46:46 +0100

    Update little about

--- components/LittleAbout.tsx
+++ components/LittleAbout.tsx
@@ -1,53 +1,56 @@
-import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
-import { FiMapPin } from "react-icons/fi";
-import { MdMail } from "react-icons/md";
+import { Github, Instagram, Linkedin, Mail, MapPin } from "lucide-react";
 
 const contacts = [
   {
     name: "GitHub",
-    icon: <FaGithub />,
+    icon: <Github />,
     href: "https://github.com/florixak",
   },
   {
     name: "E-Mail",
-    icon: <MdMail />,
+    icon: <Mail />,
     href: "mailto:ondrej@ptakondrej.dev",
   },
   {
     name: "LinkedIn",
-    icon: <FaLinkedin />,
-    href: "https://www.linkedin.com/in/ondrej-ptak-1a4b3a1b8/",
+    icon: <Linkedin />,
+    href: "https://www.linkedin.com/in/ond%C5%99ej-pt%C3%A1k-0ba354347/",
   },
   {
     name: "Instagram",
-    icon: <FaInstagram />,
+    icon: <Instagram />,
     href: "https://www.instagram.com/florixak/",
   },
 ];
 
 const LittleAbout = () => {
   return (
-    <section className="flex flex-col gap-4 p-4 rounded-lg border-foreground/50 border mx-auto max-w-2xl text-foreground ">
+    <section className="flex flex-col gap-4 p-4 rounded-lg border-foreground/20 border mx-auto max-w-2xl text-foreground">
       <div className="flex items-center justify-between">
         <div className="flex flex-col gap-1">
-          <h1 className="text-2xl font-semibold">Ondřej Pták</h1>
-          <p className="flex items-center gap-2 text-sm text-muted">
-            <FiMapPin /> Pilsen, Czech Republic
+          <h1 className="text-3xl font-semibold">Ondřej Pták</h1>
+          <p className="flex items-center gap-2 text-base text-muted-foreground">
+            <MapPin /> Pilsen, Czech Republic
           </p>
         </div>
         <div className="flex gap-4 text-3xl">
           {contacts.map((contact) => (
-            <a key={contact.name} href={contact.href}>
+            <a key={contact.name} href={contact.href} target="_blank">
               {contact.icon}
             </a>
           ))}
         </div>
       </div>
-      <p>
-        I am a 19 year old Czech Front-end Web Developer who has been interested
-        in coding since 8th grade. <br /> Currently, I am in my last year of
-        high school, specializing in application development
-      </p>
+      <ul className="flex flex-col gap-2 text-base list-disc list-outside ml-5">
+        <li>
+          I am a 19 year old Czech Front-end Web Developer who has been
+          interested in coding since 8th grade.
+        </li>
+        <li>
+          Currently, I am in my last year of high school, specializing in
+          application development.
+        </li>
+      </ul>
     </section>
   );
 };
