6dd49f44031d378b8e5d0a5e8842bf483a74ef98
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-29 20:47:26 +0100

    Add theme switch

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import ThemeSwitch from "./ThemeSwitch";
 
 const links = [
   { name: "Home", href: "/" },
@@ -11,12 +12,13 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 h-18 w-full bg-background text-foreground flex justify-between items-center px-12 py-4">
       <h1 className="font-bold">ptakondrej.dev</h1>
-      <nav className="flex gap-8">
+      <nav className="flex gap-8 items-center">
         {links.map((link) => (
           <Link key={link.name} href={link.href} className="nav-link">
             {link.name}
           </Link>
         ))}
+        <ThemeSwitch />
       </nav>
     </header>
   );
