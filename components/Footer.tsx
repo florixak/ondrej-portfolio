304ffe223b4849db5507da1f6c3cdfe09e4585b8
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-29 20:47:05 +0100

    Create footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex items-center justify-center w-full h-16">
+      <p className="max-w-46 text-center">Made with ❤ by Ondřej in Pilsen</p>
+    </footer>
+  );
 };
 
 export default Footer;
