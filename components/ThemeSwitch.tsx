0e8793e9d7695603505cc9ab52524df2b1a5f53c
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-29 20:45:15 +0100

    Create theme provider and switch

---
+++ components/ThemeSwitch.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+// import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+const ThemeSwitch = () => {
+  const { theme, setTheme } = useTheme();
+
+  const [mounted, setMounted] = useState(false);
+
+  // Only show the UI after component has mounted to avoid hydration errors
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const handleThemeChange = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  if (!mounted) {
+    return <button />;
+  }
+
+  return (
+    <button
+      className="bg-background cursor-pointer"
+      onClick={handleThemeChange}
+    >
+      {theme === "dark" ? (
+        <span className="text-2xl">☀️</span>
+      ) : (
+        <span className="text-xl">🌙</span>
+      )}
+    </button>
+    // <button className="bg-background" onClick={handleThemeChange}>
+    //   {theme === "dark" ? (
+    //     <span className="text-2xl">
+    //       <Moon />
+    //     </span>
+    //   ) : (
+    //     <span className="text-2xl">
+    //       <Sun />
+    //     </span>
+    //   )}
+    // </button>
+  );
+};
+
+export default ThemeSwitch;
