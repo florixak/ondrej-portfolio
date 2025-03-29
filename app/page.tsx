9efcd66d8b01ae9a57b82013eac4a7eb5d56b7f6
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-29 20:48:11 +0100

    Add skills to home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import LittleAbout from "@/components/LittleAbout";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
     <>
       <LittleAbout />
+      <Skills />
     </>
   );
 }
