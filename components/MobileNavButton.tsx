0f1d1cdb9a6f0bc2bf1b56a59389f5dfbc2f8a53
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-30 22:08:19 +0200

    Create mobile nav button

---
+++ components/MobileNavButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
+
+type MobileNavButtonProps = {
+  handleOpenMenu: () => void;
+};
+
+const MobileNavButton = ({ handleOpenMenu }: MobileNavButtonProps) => {
+  return (
+    <Button variant="outline" onClick={handleOpenMenu} className="z-[60]">
+      <Menu />
+    </Button>
+  );
+};
+
+export default MobileNavButton;
