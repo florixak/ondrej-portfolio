6cf50f1369afa3beda18ea6ff4d0b99af2a6c39d
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-30 11:49:46 +0200

    Add work and experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
+import CardTilt from "@/components/CardTilt";
 import LittleAbout from "@/components/LittleAbout";
 import Skills from "@/components/Skills";
+import { Briefcase, CodeSquare } from "lucide-react";
 
 export default function Home() {
   return (
     <>
       <LittleAbout />
       <Skills />
+      <section className="flex gap-4 justify-center items-center mt-10 flex-wrap">
+        <CardTilt
+          title="Work & Projects"
+          description="Here are some of my projects that I have worked on in the past."
+          image={<CodeSquare className="card-image" />}
+          href="/work"
+        />
+        <CardTilt
+          title="Experience"
+          description="Here are some of my experiences in the field of web development."
+          image={<Briefcase className="card-image" />}
+          href="/experience"
+        />
+      </section>
     </>
   );
 }
