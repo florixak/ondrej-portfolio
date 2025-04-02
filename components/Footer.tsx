19cd3feffe2b94c48f01a1c4ac29d56aa79a26c5
ptakondrej <ptakondrej@spseplzen.cz>
2025-04-02 22:13:56 +0200

    Add translations

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,10 @@
-const Footer = () => {
+import { getTranslations } from "next-intl/server";
+
+const Footer = async () => {
+  const t = await getTranslations("footer");
   return (
     <footer className="flex items-center justify-center w-full h-16">
-      <p className="max-w-46 text-center">Made with ❤ by Ondřej in Pilsen</p>
+      <p className="max-w-48 text-center">{t("text")}</p>
     </footer>
   );
 };
