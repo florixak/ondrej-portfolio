42d6ea3b366bf48126be0e80631639f220ce61e0
ptakondrej <ptakondrej@spseplzen.cz>
2025-04-14 18:11:45 +0200

    Edit robots

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      // disallow: "/api",
+      disallow: [],
     },
     sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
   };
