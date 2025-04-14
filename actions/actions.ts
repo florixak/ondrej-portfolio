c9f859d2112112cfdbcac1b279b22b65ed9014dc
ptakondrej <ptakondrej@spseplzen.cz>
2025-04-14 21:26:20 +0200

    Import new images

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,7 +1,11 @@
 "use server";
 
 import { Project } from "@/types/types";
-import BuffetImage from "@/assets/buffet.png";
+import BuffetImage from "@/assets/buffet/buffet.png";
+import BuffetImage2 from "@/assets/buffet/buffet2.png";
+import BuffetImage3 from "@/assets/buffet/buffet3.png";
+import BuffetImage4 from "@/assets/buffet/buffet4.png";
+import BuffetImage5 from "@/assets/buffet/buffet5.png";
 import { notFound } from "next/navigation";
 import { getTranslations } from "next-intl/server";
 
@@ -15,7 +19,13 @@ const projects = (t: any): Project[] => [
     technologies: ["React", "TypeScript", "TailwindCSS"],
     featured: true,
     url: "https://buffet.vlastas.cc/",
-    images: [BuffetImage],
+    images: [
+      BuffetImage,
+      BuffetImage2,
+      BuffetImage3,
+      BuffetImage4,
+      BuffetImage5,
+    ],
     github: "https://github.com/Web-Projekty/buffet-rezervace",
     createdAt: "2024-09-13",
     lastUpdated: "2024-09-13",
