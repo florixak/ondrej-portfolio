6dff56dec542162c56aa78c6340b56bfcbe6606f
ptakondrej <ptakondrej@spseplzen.cz>
2025-04-16 22:10:12 +0200

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,8 @@ import { getTranslations } from "next-intl/server";
 const Footer = async () => {
   const t = await getTranslations("footer");
   return (
-    <footer className="flex items-center justify-center w-full h-16">
+    <footer className="flex items-center justify-center flex-col w-full h-16">
+      <p>© {new Date().getFullYear()} Ondřej Pták</p>
       <p className="max-w-48 text-center">{t("text")}</p>
     </footer>
   );
