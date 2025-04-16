47a2de690cffbde79479387f1a8d4dee1de288ea
ptakondrej <florixakcz@seznam.cz>
2025-04-16 16:47:50 +0200

    Add experience data and functions

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { Project } from "@/types/types";
+import { Experience, Project } from "@/types/types";
 import BuffetImage from "@/assets/buffet/buffet.png";
 import BuffetImage2 from "@/assets/buffet/buffet2.png";
 import BuffetImage3 from "@/assets/buffet/buffet3.png";
@@ -8,6 +8,8 @@ import BuffetImage4 from "@/assets/buffet/buffet4.png";
 import BuffetImage5 from "@/assets/buffet/buffet5.png";
 import { notFound } from "next/navigation";
 import { getTranslations } from "next-intl/server";
+import { School, Star } from "lucide-react";
+import React from "react";
 
 const projects = (t: any): Project[] => [
   {
@@ -29,6 +31,33 @@ const projects = (t: any): Project[] => [
     github: "https://github.com/Web-Projekty/buffet-rezervace",
     createdAt: "2024-09-13",
     lastUpdated: "2024-09-13",
+  },
+];
+
+const experience = (t: any): Experience[] => [
+  {
+    id: "1",
+    date: "2020 - present",
+    title: t("ownprojects.title"),
+    subtitle: t("ownprojects.subtitle"),
+    description: t("ownprojects.description"),
+    icon: React.createElement(Star),
+  },
+  {
+    id: "2",
+    date: "2021 - present",
+    title: t("education.highschool.title"),
+    subtitle: t("education.highschool.subtitle"),
+    description: t("education.highschool.description"),
+    icon: React.createElement(School),
+  },
+  {
+    id: "3",
+    date: "2024 - present",
+    title: t("projects.buffet.title"),
+    subtitle: t("projects.buffet.subtitle"),
+    description: t("projects.buffet.description"),
+    icon: React.createElement(School),
   },
 ];
 
@@ -72,4 +101,23 @@ export const getProjectById = async (id: string) => {
   }
 
   return project;
+};
+
+export const getExperience = async () => {
+  const t = await getTranslations("experience");
+  const experienceList = experience(t);
+
+  return experienceList;
+};
+
+export const getExperienceById = async (id: string) => {
+  const t = await getTranslations("experience");
+  const experienceList = experience(t);
+  const exp = experienceList.find((exp) => exp.title === id);
+
+  if (!exp) {
+    notFound();
+  }
+
+  return exp;
 };
