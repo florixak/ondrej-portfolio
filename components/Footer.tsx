c018eb5e57950b6876f21a72b21e10bffc178f66
ptakondrej <ptakondrej@spseplzen.cz>
2025-04-17 21:02:37 +0200

    Edit footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,8 @@ const Footer = async () => {
   const t = await getTranslations("footer");
   return (
     <footer className="flex items-center justify-center flex-col w-full h-16">
+      <p className="text-center">{t("text")}</p>
       <p>© {new Date().getFullYear()} Ondřej Pták</p>
-      <p className="max-w-48 text-center">{t("text")}</p>
     </footer>
   );
 };
