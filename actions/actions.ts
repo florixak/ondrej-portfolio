6fac802431803bd3cd09396222318a1292623f5a
ptakondrej <ptakondrej@spseplzen.cz>
2025-04-21 20:04:33 +0200

    Move data to data file

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,74 +1,8 @@
 "use server";
 
-import { Experience, Project } from "@/types/types";
-import BuffetImage from "@/assets/buffet/buffet.png";
-import BuffetImage2 from "@/assets/buffet/buffet2.png";
-import BuffetImage3 from "@/assets/buffet/buffet3.png";
-import BuffetImage4 from "@/assets/buffet/buffet4.png";
-import BuffetImage5 from "@/assets/buffet/buffet5.png";
 import { notFound } from "next/navigation";
 import { getTranslations } from "next-intl/server";
-import { School, Star } from "lucide-react";
-import React from "react";
-
-const projects = (t: any): Project[] => [
-  {
-    id: "1",
-    title: t("buffet.title"),
-    translationTitle: "buffet",
-    description: t("buffet.description"),
-    detailedDescription: t("buffet.detailedDescription"),
-    technologies: [
-      "React",
-      "TypeScript",
-      "TailwindCSS",
-      "Vite",
-      "Axios",
-      "Zustand",
-      "React Router",
-      "React Query",
-    ],
-    featured: true,
-    url: "https://buffet.vlastas.cc/",
-    images: [
-      BuffetImage,
-      BuffetImage2,
-      BuffetImage3,
-      BuffetImage4,
-      BuffetImage5,
-    ],
-    github: "https://github.com/Web-Projekty/buffet-rezervace",
-    createdAt: "2024-09-13",
-    lastUpdated: "2024-09-13",
-  },
-];
-
-const experience = (t: any): Experience[] => [
-  {
-    id: "1",
-    date: "2020 - present",
-    title: t("ownprojects.title"),
-    subtitle: t("ownprojects.subtitle"),
-    description: t("ownprojects.description"),
-    icon: React.createElement(Star),
-  },
-  {
-    id: "2",
-    date: "2021 - present",
-    title: t("education.highschool.title"),
-    subtitle: t("education.highschool.subtitle"),
-    description: t("education.highschool.description"),
-    icon: React.createElement(School),
-  },
-  {
-    id: "3",
-    date: "2024 - present",
-    title: t("projects.buffet.title"),
-    subtitle: t("projects.buffet.subtitle"),
-    description: t("projects.buffet.description"),
-    icon: React.createElement(School),
-  },
-];
+import { experience, projects } from "@/data/data";
 
 export const getProjects = async ({
   featured = false,
---
+++ data/data.ts
@@ -0,0 +1,71 @@
+import BuffetImage from "@/assets/buffet/buffet.png";
+import BuffetImage2 from "@/assets/buffet/buffet2.png";
+import BuffetImage3 from "@/assets/buffet/buffet3.png";
+import BuffetImage4 from "@/assets/buffet/buffet4.png";
+import BuffetImage5 from "@/assets/buffet/buffet5.png";
+import BuffetImage6 from "@/assets/buffet/buffet6.png";
+import BuffetImage7 from "@/assets/buffet/buffet7.png";
+import { Project, Experience } from "@/types/types";
+import { Star, School } from "lucide-react";
+import React from "react";
+
+export const projects = (t: any): Project[] => [
+  {
+    id: "1",
+    title: t("buffet.title"),
+    translationTitle: "buffet",
+    description: t("buffet.description"),
+    detailedDescription: t("buffet.detailedDescription"),
+    technologies: [
+      "React",
+      "TypeScript",
+      "TailwindCSS",
+      "Vite",
+      "Axios",
+      "Zustand",
+      "React Router",
+      "React Query",
+    ],
+    featured: true,
+    url: "https://buffet.vlastas.cc/",
+    images: [
+      BuffetImage,
+      BuffetImage2,
+      BuffetImage3,
+      BuffetImage4,
+      BuffetImage5,
+      BuffetImage6,
+      BuffetImage7,
+    ],
+    github: "https://github.com/Web-Projekty/buffet-rezervace",
+    createdAt: "2024-09-13",
+    lastUpdated: "2024-09-13",
+  },
+];
+
+export const experience = (t: any): Experience[] => [
+  {
+    id: "1",
+    date: "2020 - present",
+    title: t("ownprojects.title"),
+    subtitle: t("ownprojects.subtitle"),
+    description: t("ownprojects.description"),
+    icon: React.createElement(Star),
+  },
+  {
+    id: "2",
+    date: "2021 - present",
+    title: t("education.highschool.title"),
+    subtitle: t("education.highschool.subtitle"),
+    description: t("education.highschool.description"),
+    icon: React.createElement(School),
+  },
+  {
+    id: "3",
+    date: "2024 - present",
+    title: t("projects.buffet.title"),
+    subtitle: t("projects.buffet.subtitle"),
+    description: t("projects.buffet.description"),
+    icon: React.createElement(School),
+  },
+];
