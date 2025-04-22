57678fd5645fb1f3a6503af98f78d57988358401
ptakondrej <ptakondrej@spseplzen.cz>
2025-04-22 22:00:08 +0200

    Return notFound page

--- app/[locale]/[...catchAll]/page.tsx
+++ app/[locale]/[...catchAll]/page.tsx
@@ -1,11 +1,5 @@
-import { Link } from "@/i18n/routing";
+import { notFound } from "next/navigation";
 
 export default function NotFound() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
-  );
+  return notFound();
 }
