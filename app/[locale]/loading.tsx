27ad0ea3276a102d77080adfd02651d34dd8e0f5
ptakondrej <ptakondrej@spseplzen.cz>
2025-04-22 22:00:49 +0200

    Create loading page

---
+++ app/[locale]/loading.tsx
@@ -0,0 +1,11 @@
+import { getTranslations } from "next-intl/server";
+
+const Loading = () => {
+  return (
+    <div className="flex-center flex-col w-full h-[50vh] p-4 text-center">
+      <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-primary-green" />
+    </div>
+  );
+};
+
+export default Loading;
