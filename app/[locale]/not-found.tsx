652918a0e895fc330a03aae73decd4132a7d15cf
ptakondrej <ptakondrej@spseplzen.cz>
2025-04-22 22:01:19 +0200

    Edit not found page

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,11 +1,23 @@
+import AnimationWrapper from "@/components/AnimationWrapper";
+import { Button } from "@/components/ui/button";
+import { getTranslations } from "next-intl/server";
 import Link from "next/link";
 
-export default function NotFound() {
+export default async function NotFound() {
+  const t = await getTranslations("common");
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <AnimationWrapper duration={0.2} delay={0.2}>
+      <section className="flex-center flex-col w-full h-[50vh] p-4 text-center">
+        <h2 className="text-6xl font-bold text-center mb-4 italic text-foreground">
+          {t("notFound.title")}
+        </h2>
+        <p className="text-lg text-muted-foreground mb-4 max-w-md">
+          {t("notFound.description")}
+        </p>
+        <Button asChild variant="outline">
+          <Link href="/">{t("notFound.buttonText")}</Link>
+        </Button>
+      </section>
+    </AnimationWrapper>
   );
 }
