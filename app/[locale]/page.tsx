ca506d722f8f1b7d00d9872275a6d90f4293f7d8
ptakondrej <florixakcz@seznam.cz>
2025-11-24 14:14:30 +0100

    Replace CardTilt and CardContent with ShojiPanel
    Refactored the home page to use the new ShojiPanel component instead of CardTilt and CardContent for the work and experience sections. Adjusted icon props and section spacing for improved layout and consistency.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,8 @@
 import AnimationWrapper from "@/components/AnimationWrapper";
-import CardContent from "@/components/CardContent";
-import CardTilt from "@/components/CardTilt";
 import FeaturedProjects from "@/components/project/FeaturedProjects";
 import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
+import ShojiPanel from "@/components/ShojiPanel";
 import { Briefcase, CodeSquare } from "lucide-react";
 
 import { getTranslations } from "next-intl/server";
@@ -18,31 +17,29 @@ export default async function Home() {
         <Skills />
       </AnimationWrapper>
 
-      <section className="flex gap-4 justify-center items-center flex-wrap mt-8">
+      <section className="flex gap-4 justify-center items-center flex-wrap my-16">
         <AnimationWrapper delay={0.4}>
-          <CardTilt href="/work">
-            <CardContent
-              title={t("work.title")}
-              description={t("work.description")}
-              image={<CodeSquare className="card-image" />}
-              buttonText={t("work.buttonText")}
-            />
-          </CardTilt>
+          <ShojiPanel
+            href="/work"
+            title={t("work.title")}
+            description={t("work.description")}
+            icon={<CodeSquare size={48} />}
+            buttonText={t("work.buttonText")}
+          />
         </AnimationWrapper>
 
         <AnimationWrapper delay={0.6}>
-          <CardTilt href="/experience">
-            <CardContent
-              title={t("experience.title")}
-              description={t("experience.description")}
-              image={<Briefcase className="card-image" />}
-              buttonText={t("experience.buttonText")}
-            />
-          </CardTilt>
+          <ShojiPanel
+            href="/experience"
+            title={t("experience.title")}
+            description={t("experience.description")}
+            icon={<Briefcase size={48} />}
+            buttonText={t("experience.buttonText")}
+          />
         </AnimationWrapper>
       </section>
 
-      <AnimationWrapper delay={0.8}>
+      <AnimationWrapper delay={0.2}>
         <FeaturedProjects />
       </AnimationWrapper>
     </>
