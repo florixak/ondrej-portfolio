4e050189f3735c8f3fcdb2707e73d61196e0a55a
ptakondrej <florixakcz@seznam.cz>
2025-11-24 14:16:46 +0100

    Remove unused GSAP imports from CardTilt
    Eliminated unused imports of gsap and useGSAP from CardTilt.tsx to clean up the code and reduce unnecessary dependencies.

--- components/CardTilt.tsx
+++ components/CardTilt.tsx
@@ -4,9 +4,6 @@ import { cn } from "@/lib/utils";
 import React, { useRef, useState } from "react";
 import { Link } from "@/i18n/routing";
 
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-
 type TiltCardProps = {
   children: React.ReactNode;
   href?: string;
