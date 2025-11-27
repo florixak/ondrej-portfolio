a5f1526339288d3c575ce8eeb2df92cf89ab3f97
ptakondrej <florixakcz@seznam.cz>
2025-11-27 08:04:38 +0100

    Update Footer layout and styling
    Changed the Footer component to use a flex row layout with evenly spaced items, increased height, and applied muted foreground text color for improved appearance.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { getTranslations } from "next-intl/server";
 const Footer = async () => {
   const t = await getTranslations("footer");
   return (
-    <footer className="flex items-center justify-center flex-col w-full h-16">
+    <footer className="flex items-center justify-evenly flex-row w-full h-22 text-muted-foreground">
       <p className="text-center">{t("text")}</p>
       <p>© {new Date().getFullYear()} Ondřej Pták</p>
     </footer>
