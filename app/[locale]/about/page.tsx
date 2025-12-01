0bdc31f1f297a530284d074c538ea8d150795cfa
ptakondrej <florixakcz@seznam.cz>
2025-12-01 21:37:20 +0100

    Refactor About page to use async translations
    Updated AboutPage component to be async and fetch translations using getTranslations. Removed hardcoded text content in preparation for localization.

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -7,27 +7,12 @@ export const generateMetadata = async () => {
   };
 };
 
-const AboutPage = () => {
+const AboutPage = async () => {
+  const t = await getTranslations("about");
   return (
     <section className="flex-center flex-col gap-4 mt-10">
       <h1>Let's talk about my journey</h1>
-      <div className="flex flex-col gap-4 max-w-2xl border border-foreground/20 p-4 rounded-lg">
-        <p>
-          Everything started when I was 14 year old in elementary school, my
-          online friend created Minecraft server and he introduced me to the
-          world of programming. I was fascinated by how you could create your
-          own plugins and minigames. So I started learning Java, and I was
-          hooked in.
-        </p>
-        <p>
-          I instatly know what high school should I choose and what I want to do
-          in my future.
-        </p>
-        <p>
-          In high school I was introduced to web development. In the beginning I
-          did not understand how layout, media queries and more work.
-        </p>
-      </div>
+      <div className="flex flex-col gap-4 max-w-2xl border border-foreground/20 p-4 rounded-lg"></div>
     </section>
   );
 };
